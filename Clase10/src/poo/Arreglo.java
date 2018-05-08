810132ee2e2cf17acb8ecccf48e236cffcf2d8d4
curruman <37887061+curruman@users.noreply.github.com>
2018-05-07 21:50:15 -0300

    Add files via upload

---
+++ Clase10/src/poo/Arreglo.java
@@ -0,0 +1,19 @@
+package poo;
+
+import java.util.Scanner;
+
+public class Arreglo {
+	
+	public void ejercicio() {
+		String arreglo [] = new String[5] ;
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i< arreglo.length; i++) {
+			arreglo[i]= sc.next();	
+		}
+		for(int i = 0; i<arreglo.length; i++) {
+			System.out.println(arreglo[i] + " Tamaño " + arreglo[i].length());
+		}
+		sc.close();
+	}
+
+}
